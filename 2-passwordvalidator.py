a143f7d33a0771d87599c1d24a332c71c94558e6
unknown <giancarloestrella09@gmail.com>
2021-11-27 23:56:00 +0800

    Checks the given password if it fits the given criteria.

---
+++ 2-passwordvalidator.py
@@ -0,0 +1,18 @@
+# Create a program that check if password is valid
+# The password is valid if all criteria are met:
+# a. Greater than 15 letters
+# b. Have at least one capital letter
+# c. Have at least one number
+# d. Have at least one special char (!@#$%^&*()_+ etc)
+# ex.
+# input: P@ssw0rd+P@ssw0rd
+# output: Valid
+# Tip: loop through each character of the input then process it letter by letter
+
+# steps:
+# 1. ask for password
+password = input("Enter your password here: ")
+
+# 2. check for capital letters, numbers, special characters
+
+# 3. display if password is valid or not
