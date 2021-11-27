ed7bea15ddc2efcafdc8127e7fddf340a972a440
unknown <giancarloestrella09@gmail.com>
2021-11-27 22:34:47 +0800

    Counts the number of words, vowels, and consonants in a user-given sentence.

---
+++ 1-charcounter.py
@@ -0,0 +1,14 @@
+# Create a program that ask for a sentence as input.
+# Display the number of words, vowels and consonants in the input 
+# ex.
+# input: Bahala kayo dyan
+# output:
+# words: 3
+# vowels: 6
+# consonants: 8
+
+# steps:
+# 1. ask for a sentence
+sentence = input("Place your sentence here: ")
+# 2. count the number of words, vowels, and consonants
+# 3. display
